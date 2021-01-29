4848dd06bfac6c0d5abf85b1307eade8ba0eaaac
Ivan <ivan.aldush@gmail.com>
2021-01-30 00:50:22 +0300

    add more analytics

--- analytics/functions.js
+++ analytics/functions.js
@@ -1,4 +1,15 @@
-const { sortBy, prop, flatten } = require('ramda')
+const { sortBy, prop, flatten, head, last, uniq } = require('ramda')
+const cliProgress = require('cli-progress');
+const natural = require('natural')
+const wordsInBlackList = require('stopwords-ru') 
+const dayjs = require('dayjs')
+const sentimentalVocab = require('./vocab.json')
+require('dayjs/locale/ru')
+
+const sentimental = require('Sentimental');
+
+const { WordTokenizer } = natural;
+const tokenizer = new WordTokenizer();
 
 function getPeoplesByMessages(messages) {
     let peoples = {};
@@ -9,7 +20,6 @@ function getPeoplesByMessages(messages) {
 
     return peoples
 }
-
 
 function getMessagesCountByPeople(messages) {
     let peoples = getPeoplesByMessages(messages)
@@ -39,6 +49,7 @@ function normalizeMessage(text){
         return text
         .toLowerCase()
         .replace(/[^a-zA-Zа-яА-я\s]+/g, '')
+        .replace(/\n/g, '')
         .replace('  ', ' ')
         .trim()
     }
@@ -46,36 +57,264 @@ function normalizeMessage(text){
     return ''
 }
 
+function createCliProgressBar(processName) {
+    return new cliProgress.SingleBar({
+        format: `${processName} | {bar} | {percentage}% || {value}/{total}`,
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
+    });
+}
+
+function wordIsTrash(word){
+    return wordsInBlackList.includes(word)
+}
+
+function tokenizeWords(text){
+    return tokenizer.tokenize(text);
+}
+
 function getTopByWords(messages) {
+    const b1 = createCliProgressBar('Messages normalization')
+    const b2 = createCliProgressBar('Analize word count    ')
+    const b3 = createCliProgressBar('Format statistic      ')
+
     let words = {}
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
     let normalMessages = messages
-    .map(item => normalizeMessage(item.text))
+    .map((item, index) => { 
+        b1.update(index + 1);
+        return normalizeMessage(item.text)
+    })
     .filter(item => item !== '')
 
-    let preparedWords = flatten(normalMessages.map(item => item.split(' ')))
+    let preparedWords = flatten(normalMessages.map(tokenizeWords))
 
-    preparedWords.forEach(word => {
+    b1.stop();
+
+    b2.start(preparedWords.length, 0, {
+        speed: "N/A"
+    });
+    preparedWords.forEach((word, index) => {
+        if (wordIsTrash(word)) {
+            return
+        }
+
         if (Object.keys(words).includes(word)) {
             words[word] = words[word] + 1
         } else {
             words[word] = 1
         }
+        b2.update(index + 1);
+    });
+    b2.stop();
+
+    b3.start(Object.keys(words).length, 0, {
+        speed: "N/A"
     });
 
-    let wordsData = Object.keys(words).map(key => ({
-        word: key,
-        count: words[key]
-    }))
+    let wordsData = Object.keys(words).map((key, index) => {
+        b3.update(index + 1)
+        return {
+            word: key,
+            count: words[key]
+        }
+    })
 
     wordsData = sortBy(prop('count'), wordsData)
+
+    b3.stop();
 
     return wordsData
 }
 
+function getMessagesCountByDate(messages){
+    let dates = {}
+    let b1 = createCliProgressBar('Analize messages count by date')
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
+    messages.forEach((message, index) => {
+        let date = head(message.date.split('T'))
+
+        if (Object.keys(dates).includes(date)) {
+            dates[date] = dates[date] + 1
+        } else {
+            dates[date] = 1
+        }
+
+        b1.update(index + 1)
+    })
+
+    b1.stop();
+
+    datesData = Object.keys(dates).map(key => ({
+        date: key,
+        messages: dates[key]
+    }))
+
+    datesData = sortBy(prop('messages'), datesData)
+
+    return datesData
+}
+
+function getMessagesCountByHour(messages){
+    let hours = {}
+    let b1 = createCliProgressBar('Analize messages count by hour')
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
+    messages.forEach((message, index) => {
+        let hour = head(last(message.date.split('T')).split(':'))
+
+        if (Object.keys(hours).includes(hour)) {
+            hours[hour] = hours[hour] + 1
+        } else {
+            hours[hour] = 1
+        }
+
+        b1.update(index + 1)
+    })
+
+    b1.stop();
+
+    hoursData = Object.keys(hours).map(key => ({
+        hour: key,
+        messages: hours[key]
+    }))
+
+    hoursData = sortBy(prop('messages'), hoursData)
+
+    return hoursData
+}
+
+function getMessagesCountByDay(messages){
+    let days = {}
+    let b1 = createCliProgressBar('Analize messages count by day')
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
+    messages.forEach((message, index) => {
+        let day = dayjs(message.date).locale('ru').format('dddd')
+
+        if (Object.keys(days).includes(day)) {
+            days[day] = days[day] + 1
+        } else {
+            days[day] = 1
+        }
+
+        b1.update(index + 1)
+    })
+
+    b1.stop();
+
+    daysData = Object.keys(days).map(key => ({
+        day: key,
+        messages: days[key]
+    }))
+
+    daysData = sortBy(prop('messages'), daysData)
+
+    return daysData
+}
+
+function analizeWord(word){
+    let hasTon = sentimentalVocab.hasOwnProperty(word)
+
+    if (hasTon) {
+        return sentimentalVocab[word]
+    }
+
+    return 0
+}
+
+function getTonByMessage(message) {
+    const normilizedMessage = normalizeMessage(message)
+    const words = tokenizeWords(normilizedMessage)
+
+    let ton = 0;
+    let hits = 0;
+
+    words.forEach(word => {
+        let score = analizeWord(word)
+        ton += score
+        if (score !== 0) {
+            hits+=1
+        }
+    })
+
+    return {
+        score: ton,
+        hits,
+        compatibility: hits/words.length * 100
+    }
+}
+
+function extractMessagesText(messages) {
+    let b1 = createCliProgressBar('Extract messages to raw format')
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
+    let rawMessages = messages.map((message, index) => {
+        b1.update(index + 1)
+
+        if (typeof message.text === 'string') {
+            return message.text
+        }
+
+        return ''
+    }).filter(item => String(item).trim() !== '')
+
+    b1.stop();
+
+    return rawMessages
+}
+
+function extractMessagesWordsText(messages) {
+    let b1 = createCliProgressBar('Extract messages words to raw format')
+
+    b1.start(messages.length, 0, {
+        speed: "N/A"
+    });
+
+    let rawMessages = flatten(messages.map((message, index) => {
+        b1.update(index + 1)
+
+        if (typeof message.text === 'string') {
+            return tokenizeWords(message.text)
+        }
+
+        return []
+    })).filter(it => !wordIsTrash(it))
+
+    b1.stop();
+
+    return uniq(rawMessages)
+}
 
 module.exports = {
     getPeoplesByMessages,
     getMessagesCountByPeople,
     normalizeMessage,
-    getTopByWords
+    getTopByWords,
+    tokenizeWords,
+    wordIsTrash,
+    getMessagesCountByDate,
+    getMessagesCountByHour,
+    getMessagesCountByDay,
+    getTonByMessage,
+    extractMessagesText,
+    extractMessagesWordsText,
 }
--- analytics/index.js
+++ analytics/index.js
@@ -1,10 +1,20 @@
 const data = require('./result.json')
+const fs = require('fs');
 const { head, last, take, takeLast } = require('ramda')
 const dayjs = require('dayjs')
-const { getPeoplesByMessages, getMessagesCountByPeople, getTopByWords } = require('./functions')
+const { getPeoplesByMessages, 
+    getMessagesCountByPeople, 
+    getTopByWords, 
+    getMessagesCountByDate, 
+    getMessagesCountByHour, 
+    getMessagesCountByDay, 
+    getTonByMessage, 
+    extractMessagesText, 
+    extractMessagesWordsText
+} = require('./functions')
 
 const messages = data.messages;
-// const messages = take(100, data.messages);
+// const messages = take(5, data.messages);
 
 const firstMessageDate = new Date(head(messages).date)
 const lastMessageDate = new Date(last(messages).date)
@@ -20,13 +30,43 @@ console.log('------------------------------------------------');
 let members = {};
 let messagesPerPeopleData = []
 let topByWords = []
+let messagesByDate = []
+let messagesByHour = []
+let messagesByDay = []
 
 let justMessages = messages.filter(item => item.type === 'message')
 
-members = getPeoplesByMessages(justMessages)
-messagesPerPeopleData = getMessagesCountByPeople(justMessages)
-topByWords = getTopByWords(messages)
+// members = getPeoplesByMessages(justMessages)
+// messagesPerPeopleData = getMessagesCountByPeople(justMessages)
+// topByWords = takeLast(100, getTopByWords(justMessages))
+// messagesByDate = takeLast(100, getMessagesCountByDate(justMessages))
+// messagesByHour = takeLast(100, getMessagesCountByHour(justMessages))
+// messagesByDay = takeLast(100, getMessagesCountByDay(justMessages))
+// console.log('members', members);
+// console.log('messagesPerMembers', messagesPerPeopleData);
+// console.log('topByWords', topByWords);
+// console.log('messagesByDate', messagesByDate);
+// console.log('messagesByHour', messagesByHour);
+// console.log('messagesByDay', messagesByDay);
 
-console.log('members', members);
-console.log('messagesPerMembers', messagesPerPeopleData);
-console.log('topByWords', takeLast(20, topByWords));
+// fs.writeFile("topByWords.json", JSON.stringify(topByWords), function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+// let rawMessages = extractMessagesText(justMessages)
+// let rawWords = extractMessagesWordsText(justMessages)
+
+// fs.writeFile("extractMessagesText.json", JSON.stringify(rawMessages), function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+
+// fs.writeFile("extractWordsText.json", JSON.stringify(rawWords), function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
